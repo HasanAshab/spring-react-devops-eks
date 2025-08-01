cc399c58d740077381720d14f04c83e84fb79256
Your Name <you@example.com>
2025-08-01 10:53:51 +0600

    backup

--- frontend/src/api/customAxiosConfig/CustomAxiosConfig.js
+++ frontend/src/api/customAxiosConfig/CustomAxiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // axios instance for making requests
 const axiosInstance = axios.create({
-  baseURL: "http://backend:8000",
+  baseURL: "http://backend:8080",
 });
 
 // request interceptor for adding token
